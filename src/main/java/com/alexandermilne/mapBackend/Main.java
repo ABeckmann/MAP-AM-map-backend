6a48fb5c15cf5e6c22ddb43d742f316b8e5db789
alex <alexander.j.m.milne@gmail.com>
2020-05-18 12:13:17 +0100

    .

--- src/main/java/com/alexandermilne/mapBackend/Main.java
+++ src/main/java/com/alexandermilne/mapBackend/Main.java
@@ -1,16 +1,20 @@
 package com.alexandermilne.mapBackend;
 
-import com.alexandermilne.mapBackend.filestore.local.storage.StorageService;
+import com.alexandermilne.mapBackend.filestore.local.storage.FileSystemStorageService;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
+import com.alexandermilne.mapBackend.filestore.local.storage.StorageService;
+import com.alexandermilne.mapBackend.filestore.local.storage.StorageProperties;
+
 @SpringBootApplication
-@ComponentScan({"com.delivery.request"})
-@EntityScan("com.delivery.domain")
+@EnableConfigurationProperties(StorageProperties.class)
 public class Main {
 
 	public static void main(String[] args) {
