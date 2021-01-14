d698839d503c2ad285b0f45b70f95a53ce55546d
Matheus Torquato <matheusft@gmail.com>
2021-01-14 10:36:59 -0300

    New renamed files

---
+++ src/main/java/com/alexandermilne/mapBackend/config/PostgresConfig.java
@@ -0,0 +1,29 @@
+package com.alexandermilne.mapBackend.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PostgresConfig {
+
+    // @ConfigurationProperties(prefix = "spring.datasource")
+    //  @Bean
+    //  @Primary
+    //  public DataSource dataSource(String username,String password) {
+    //     return DataSourceBuilder.create().username(username).password(password).build();
+    //  }
+    @Bean
+    @ConfigurationProperties("app.datasource")
+    public HikariDataSource hikariDataSource(){
+        return DataSourceBuilder
+                .create()
+                .username("postgres")
+                .password("password")
+                .type(HikariDataSource.class)
+                .build();
+    }
+
+}
